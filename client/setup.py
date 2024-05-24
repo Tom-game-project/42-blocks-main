c0d956858b23ec88938e7b830c68667d8b373501
Runa Kabashima <rukabash@c1r5s8.42tokyo.jp>
2024-05-24 19:37:50 +0900

    edit setup.py

--- client/setup.py
+++ client/setup.py
@@ -10,7 +10,7 @@ setup(
     install_requires=install_requirements,
     entry_points={
         "console_scripts": [
-            "ss_tarou=ss_player.main:main",
+            "ss_blockfighter=ss_player.main:main",
         ]
     },
     classifiers=[
