aee9114e0160501b00265bf1149704f0d73cec0c
Runa Kabashima <rukabash@c1r5s6.42tokyo.jp>
2024-05-25 14:04:14 +0900

    impl PlayerProc.py

--- client/ss_player/PlayerProc.py
+++ client/ss_player/PlayerProc.py
@@ -22,12 +22,15 @@ def genarr(strorder:str) -> tuple[bool, np.ndarray]:
     return 
 
 def is_piece_duplicate(board:np.array,order:np.ndarray) -> bool:
-    # ピースが被っている True
-    # ピースが被っていない False
-    return
+    # 行列のなかで0以外の要素をすべて1に変換する
+    board[board != 0] = 1
+    # orderで受け取った列をボードに足す
+    board += order
+    # 2以上の要素があるかどうかをチェックする
+    return not np.any(board >= 2)
+
 
 def is_vertex_duplicate(board:np.array,order:np.ndarray) -> bool:
-    # 頂点を自分のピースと共有している
     return
 
 def is_edge_duplicate(board:np.array,order:np.ndarray) -> bool:
