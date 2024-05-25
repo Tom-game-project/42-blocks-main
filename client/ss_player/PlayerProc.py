95b8b2dfbf4bf8b6d64a5274859ce76361287c79
Tomonari Muranaka <tmuranak@c1r5s4.42tokyo.jp>
2024-05-25 14:56:54 +0900

    impl genarr func

--- client/ss_player/PlayerProc.py
+++ client/ss_player/PlayerProc.py
@@ -1,4 +1,6 @@
 import numpy as np
+from client.ss_player import Block, BlockRotation
+from client.ss_player import BlockType
 import copy
 
 # putable
@@ -9,7 +11,7 @@ def putable(board:np.ndarray,strorder:str) -> bool:
     if not check:
         return False
     if not is_piece_duplicate(board, order):
-        return False 
+        return False
     if is_vertex_duplicate(board, order):
         return False 
     if not is_edge_duplicate(board, order):
@@ -18,8 +20,26 @@ def putable(board:np.ndarray,strorder:str) -> bool:
 
 
 def genarr(strorder:str) -> tuple[bool, np.ndarray]:
-
-    return 
+    # U034
+    bt = BlockType(strorder[0])     # 一文字目
+    br = BlockRotation(strorder[1]) # 二文字目
+    x = "123456789ABCDE".find(strorder[2])
+    y = "123456789ABCDE".find(strorder[3])
+    block: np.ndarray = Block(bt,br).block_map()
+    height,width = block.shape
+    if 14 < y + height or 14 < x + width:
+        return False,None
+    block_lst = block.tolist()
+    return True,np.array([
+        [
+            block_lst[i - y][j - x]
+            if x <= j < x + width and y <= i < y + height
+            else
+                0
+            for j in range(14)
+        ]
+        for i in range(14)
+    ])        
 
 def is_piece_duplicate(board:np.array,order:np.ndarray) -> bool:
     # ピースが被っている True
